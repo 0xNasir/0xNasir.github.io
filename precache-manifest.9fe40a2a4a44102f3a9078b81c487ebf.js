97257182bcfb374f0af62c785bd295539b2f2ee4
coding by nasir <nasirkhancse@gmail.com>
2019-01-02 19:24:54 +0600

    Updates

---
+++ precache-manifest.9fe40a2a4a44102f3a9078b81c487ebf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "f10bf0780aecd8ff951259976c1784dd",
+    "url": "/static/media/pokeball.f10bf078.png"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "a54124c416fc180ceb24",
+    "url": "/static/js/main.a54124c4.chunk.js"
+  },
+  {
+    "revision": "8abbb34e241f59734c57",
+    "url": "/static/js/1.8abbb34e.chunk.js"
+  },
+  {
+    "revision": "a54124c416fc180ceb24",
+    "url": "/static/css/main.4039a778.chunk.css"
+  },
+  {
+    "revision": "4b63c42a6972262915910c56264cd0d8",
+    "url": "/index.html"
+  }
+];
